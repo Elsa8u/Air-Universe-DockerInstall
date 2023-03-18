003f81b99336381f15a7f76e686d8583d57b72e6
Hiccup6 <71973511+Hiccup6@users.noreply.github.com>
2023-03-19 01:15:50 +0800

    Update tcp-turbo.sh

--- tcp-turbo.sh
+++ tcp-turbo.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 echo "
-fs.inotify.max_user_watches = 100000
-net.ipv4.tcp_max_tw_buckets = 6000
+kernel.core_pattern=/home/cores/core.%P.%e.%t
+net.ipv4.tcp_tw_reuse = 1
+fs.inotify.max_user_watches = 1000000
+net.ipv4.tcp_max_tw_buckets = 1000000
 net.ipv4.tcp_max_syn_backlog=262144
 net.core.somaxconn=163840
-net.ipv4.tcp_syn_retries=1
+net.ipv4.tcp_syn_retries=3
 net.core.netdev_max_backlog=163840
+net.ipv4.tcp_timestamps=1
+net.ipv4.tcp_keepalive_time = 1200
 fs.file-max=11000000
 fs.nr_open=10000000
 net.ipv4.icmp_echo_ignore_all=0
@@ -18,37 +22,14 @@ net.ipv4.tcp_retries2=5
 net.ipv4.tcp_synack_retries=2
 net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=1
-net.ipv4.tcp_mem=131072  262144  524288
-net.ipv4.tcp_rmem = 4096 87380 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-
+net.ipv4.tcp_mem=764346        1019129        1528692
+net.ipv4.tcp_rmem=4096        87380        33554432
+net.ipv4.tcp_wmem=4096        65536        33554432
 net.ipv4.neigh.default.gc_thresh3=8192
 net.ipv4.neigh.default.gc_thresh2=4096
 net.ipv4.neigh.default.gc_thresh1=2048
-
-net.ipv4.tcp_slow_start_after_idle = 0
-fs.file-max = 1000000
-fs.inotify.max_user_instances = 8192
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_tw_reuse = 1
 net.ipv4.ip_local_port_range = 1024 65000
-net.ipv4.tcp_max_syn_backlog = 16384
-net.core.netdev_budget = 1000
-net.ipv4.ip_early_demux=0
-net.core.dev_weight=1024
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.ip_conntrack_max = 655360
-net.ipv4.tcp_no_metrics_save = 0
-net.ipv4.tcp_low_latency = 0
-net.ipv4.tcp_reordering = 2
-net.ipv4.tcp_orphan_retries = 1
-net.ipv4.tcp_synack_retries = 1
-net.ipv4.tcp_fack = 1
-net.ipv4.route.gc_timeout = 100
-net.core.netdev_max_backlog = 32768
-net.ipv4.tcp_max_orphans = 32768
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_rfc1337 = 0
+net.core.netdev_budget = 900
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr">/etc/sysctl.conf
 
@@ -58,11 +39,6 @@ echo "* soft nofile 1048576
 * hard nofile 1048576
 * soft nproc 1048576
 * hard nproc 1048576
-DefaultLimitNOFILE=1000000
-* soft core 1048576
-* hard core 1048576
-* hard memlock unlimited
-* soft memlock unlimited
 ">/etc/security/limits.conf
 
 sysctl -p 
