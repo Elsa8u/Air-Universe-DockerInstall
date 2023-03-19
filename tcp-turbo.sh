95613ae3fa416ffe4768786e5c3f2c517973ecdb
Hiccup6 <71973511+Hiccup6@users.noreply.github.com>
2023-03-19 15:22:19 +0800

    Update tcp-turbo.sh

--- tcp-turbo.sh
+++ tcp-turbo.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 echo "
-kernel.core_pattern=/home/cores/core.%P.%e.%t
 net.ipv4.tcp_tw_reuse = 1
 fs.inotify.max_user_watches = 1000000
 net.ipv4.tcp_max_tw_buckets = 1000000
@@ -8,7 +7,7 @@ net.ipv4.tcp_max_syn_backlog=262144
 net.core.somaxconn=163840
 net.ipv4.tcp_syn_retries=3
 net.core.netdev_max_backlog=163840
-net.ipv4.tcp_timestamps=0
+net.ipv4.tcp_timestamps=1
 net.ipv4.tcp_keepalive_time = 1200
 fs.file-max=11000000
 fs.nr_open=10000000
@@ -22,12 +21,8 @@ net.ipv4.tcp_retries2=5
 net.ipv4.tcp_synack_retries=2
 net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=1
-net.ipv4.tcp_mem=764346        1019129        1528692
-net.ipv4.tcp_rmem = 4096 65536 67108864
+net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.neigh.default.gc_thresh3=8192
-net.ipv4.neigh.default.gc_thresh2=4096
-net.ipv4.neigh.default.gc_thresh1=2048
 
 net.ipv4.ip_local_port_range = 1024 65000
 net.core.netdev_budget = 600
