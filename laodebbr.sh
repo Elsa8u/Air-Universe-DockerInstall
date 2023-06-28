31d685684ef2d8a9a3cc289940cce912b7ef3394
ElsaV <71973511+Elsa8u@users.noreply.github.com>
2023-06-28 18:08:08 +0800

    Update laodebbr.sh

--- laodebbr.sh
+++ laodebbr.sh
@@ -3,9 +3,6 @@ echo "
 net.ipv6.conf.all.disable_ipv6 = 0
 net.ipv6.conf.default.disable_ipv6 = 0
 net.ipv6.conf.lo.disable_ipv6 = 0
-root@vietnam:~# cat /etc/sysctl.d/
-100-optimize.conf  99-sysctl.conf     README.sysctl      
-root@vietnam:~# cat /etc/sysctl.d/100-optimize.conf 
 fs.file-max = 655360
 fs.inotify.max_user_instances = 8192
 net.ipv4.tcp_keepalive_time = 600
