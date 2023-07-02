5b54bacb746750ed4ac9e7fbb26174aa4fe4bea7
ElsaV <71973511+Elsa8u@users.noreply.github.com>
2023-07-02 14:11:38 +0800

    Update tcp-turbo.sh

--- tcp-turbo.sh
+++ tcp-turbo.sh
@@ -1,87 +1,86 @@
 #!/bin/sh
 echo "
-vm.swappiness = 10
-fs.file-max = 1000000
-fs.inotify.max_user_instances = 8192
-fs.pipe-max-size = 1048576
-fs.pipe-user-pages-hard = 0
-fs.pipe-user-pages-soft = 0
-net.ipv4.conf.all.rp_filter = 0
-net.ipv4.conf.default.rp_filter = 0
+net.core.rps_sock_flow_entries=32768
+kernel.pid_max=16777216
+kernel.threads-max=1000000
+vm.max_map_count=1000000
+fs.file-max=1000000
+fs.inotify.max_user_instances=131072
+net.ipv4.conf.all.route_localnet=1
+net.ipv4.ip_forward=1
+net.ipv4.conf.all.forwarding=1
+net.ipv4.conf.default.forwarding=1
 
-# socket status
-net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_fin_timeout = 30
-net.ipv4.tcp_tw_timeout = 10
-net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_timestamps = 1
-net.ipv4.tcp_keepalive_time = 1200
-net.ipv4.tcp_keepalive_probes = 3
-net.ipv4.tcp_keepalive_intvl = 15
-net.ipv4.tcp_max_syn_backlog = 8192
-net.ipv4.tcp_max_tw_buckets = 3000
-net.ipv4.route.gc_timeout = 100
-net.ipv4.tcp_syn_retries = 2
-net.ipv4.tcp_synack_retries = 2
+net.ipv6.conf.all.forwarding=1
+net.ipv6.conf.default.forwarding=1
+net.ipv6.conf.lo.forwarding=1
+net.ipv6.conf.all.disable_ipv6=0
+net.ipv6.conf.default.disable_ipv6=0
+net.ipv6.conf.lo.disable_ipv6=0
 
-# tcp window
-net.core.wmem_default = 262144
-net.core.wmem_max = 67108864
-net.core.somaxconn = 3276800
-net.core.optmem_max = 81920
-net.core.rmem_default = 262144
-net.core.rmem_max = 67108864
-net.core.netdev_max_backlog = 400000
-net.core.netdev_budget = 600
-net.ipv4.tcp_max_orphans = 3276800
+net.ipv6.conf.all.accept_ra=2
+net.ipv6.conf.default.accept_ra=2
 
-# forward
-net.ipv4.ip_forward = 1
-net.ipv6.conf.all.forwarding = 1
+net.ipv4.conf.all.accept_redirects=0
+net.ipv4.conf.default.accept_redirects=0
+net.ipv4.conf.all.secure_redirects=0
+net.ipv4.conf.default.secure_redirects=0
+net.ipv4.conf.all.send_redirects=0
+net.ipv4.conf.default.send_redirects=0
+net.ipv4.conf.default.rp_filter=0
+net.ipv4.conf.all.rp_filter=0
 
-# ipv4
+net.ipv4.tcp_syncookies=1
+net.ipv4.tcp_retries1=3
+net.ipv4.tcp_retries2=5
+net.ipv4.tcp_orphan_retries=1
+net.ipv4.tcp_syn_retries=3
+net.ipv4.tcp_synack_retries=3
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_fin_timeout=10
+net.ipv4.tcp_max_tw_buckets=262144
+net.ipv4.tcp_max_syn_backlog=4194304
+net.core.netdev_max_backlog=4194304
+net.core.somaxconn=262144
+net.ipv4.tcp_notsent_lowat=16384
+net.tcp_timestamps=1
+net.ipv4.tcp_keepalive_time=300
+net.ipv4.tcp_keepalive_probes=3
+net.ipv4.tcp_keepalive_intvl=60
+
+net.ipv4.tcp_fastopen=3
+net.ipv4.tcp_autocorking=0
+net.ipv4.tcp_slow_start_after_idle=0
 net.ipv4.tcp_no_metrics_save=1
 net.ipv4.tcp_ecn=0
 net.ipv4.tcp_frto=0
 net.ipv4.tcp_mtu_probing=0
-net.ipv4.tcp_rfc1337=0
 net.ipv4.tcp_sack=1
 net.ipv4.tcp_fack=1
 net.ipv4.tcp_window_scaling=1
 net.ipv4.tcp_adv_win_scale=1
 net.ipv4.tcp_moderate_rcvbuf=1
-net.ipv4.tcp_mem = 786432 2097152 3145728 
-net.ipv4.tcp_rmem = 4096 524288 67108864
-net.ipv4.tcp_wmem = 4096 524288 67108864
+net.core.rmem_max=33554432
+net.core.wmem_max=33554432
+net.ipv4.tcp_rmem=16384 131072 67108864
+net.ipv4.tcp_wmem=4096 16384 33554432
 net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
-net.core.default_qdisc=fq
+net.ipv4.udp_wmem_min=4096
+net.ipv4.tcp_thin_linear_timeouts=1
+net.ipv4.tcp_mem=262144 1048576 4194304
+net.ipv4.udp_mem=262144 1048576 4194304
 net.ipv4.tcp_congestion_control=bbr
+net.core.default_qdisc=fq
+net.ipv4.ip_local_port_range=2048 25000
+net.ipv4.ping_group_range=0 2147483647
+net.netfilter.nf_conntrack_max=67108864
+net.netfilter.nf_conntrack_buckets=16777216
 
-# netfiliter iptables
-net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 30
-net.netfilter.nf_conntrack_tcp_timeout_time_wait = 30
-net.netfilter.nf_conntrack_tcp_timeout_close_wait = 15
-net.netfilter.nf_conntrack_tcp_timeout_established = 350
-net.netfilter.nf_conntrack_max = 25000000
-net.netfilter.nf_conntrack_buckets = 25000000
+net.ipv4.tcp_collapse_max_bytes=67108864
+
 net.core.netdev_budget = 600
 ">/etc/sysctl.conf
 
-echo "* soft nofile 1048576
- DefaultLimitNPROC=1000000
- root hard nofile 1048576
-* hard nofile 1048576
-* soft nproc 1048576
-* hard nproc 1048576
-DefaultLimitNOFILE=1000000
-* soft core 1048576
-* hard core 1048576
-* hard memlock unlimited
-* soft memlock unlimited
-">/etc/security/limits.conf
-
 sysctl -p 
-sysctl --system 
 
 echo "优化完成, 请重启系统生效 (OpenVZ/LXC 架构可能效果不佳 建议升级到5.10以上的内核并开启BBR)"
